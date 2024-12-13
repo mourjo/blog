0a2c03879c0999b4af550df6f461e708cb8c70e8
Mourjo Sen <hello@mourjo.me>
2024-12-13 16:41:50 +0100

    Pubish script - changes

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+git fetch origin;
+
 rm -rf /tmp/_site ; 
 mv _site /tmp/_site ;
 git checkout gh-pages ;
 cp -R /tmp/_site/* . ;
 rm -rf hidden
-rm Gemfile;
-rm Gemfile.lock;
-rm LICENCE.md
+rm -rf Gemfile;
+rm -rf Gemfile.lock;
+rm -rf LICENCE.md;
