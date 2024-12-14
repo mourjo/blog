ef99c8d8428dfa0b9129a65e38a4e00e874c1036
Mourjo Sen <hello@mourjo.me>
2024-12-14 13:57:28 +0100

    Update publish.sh

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 git fetch origin;
-
+bundle exec jekyll b;
 TMP_DIR="/tmp/blog/$(date  +%Y_%m_%d_%H_%M_%S)__$(git rev-parse --short HEAD)"
-mv _site TMP_DIR
+mkdir -p "$TMP_DIR"
+mv _site "$TMP_DIR"
 git checkout gh-pages
-echo cp -R "/tmp/blog/$TMP_DIR/*" .
+cp -r "$TMP_DIR/_site/" .
 rm -rf hidden
 rm -rf Gemfile
 rm -rf Gemfile.lock
