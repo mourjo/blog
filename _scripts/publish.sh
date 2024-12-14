ca2dcb63ebc0c54766426358c8cdd46216d5285f
Mourjo Sen <hello@mourjo.me>
2024-12-14 13:56:01 +0100

    Test echo

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -5,7 +5,7 @@ git fetch origin;
 TMP_DIR="/tmp/blog/$(date  +%Y_%m_%d_%H_%M_%S)__$(git rev-parse --short HEAD)"
 mv _site TMP_DIR
 git checkout gh-pages
-cp -R "/tmp/blog/$TMP_DIR/*" .
+echo cp -R "/tmp/blog/$TMP_DIR/*" .
 rm -rf hidden
 rm -rf Gemfile
 rm -rf Gemfile.lock
