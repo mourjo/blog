df872a2650aba5e138f2c39e11be4dddcd3d11e9
Mourjo Sen <hello@mourjo.me>
2024-12-14 13:49:25 +0100

    Use new directory while copying to tmp

--- _scripts/publish.sh
+++ _scripts/publish.sh
@@ -2,11 +2,11 @@
 
 git fetch origin;
 
-rm -rf /tmp/_site ; 
-mv _site /tmp/_site ;
-git checkout gh-pages ;
-cp -R /tmp/_site/* . ;
+TMP_DIR="/tmp/blog/$(date  +%Y_%m_%d_%H_%M_%S)__$(git rev-parse --short HEAD)"
+mv _site TMP_DIR
+git checkout gh-pages
+cp -R "/tmp/blog/$TMP_DIR/*" .
 rm -rf hidden
-rm -rf Gemfile;
-rm -rf Gemfile.lock;
-rm -rf LICENCE.md;
+rm -rf Gemfile
+rm -rf Gemfile.lock
+rm -rf LICENCE.md
